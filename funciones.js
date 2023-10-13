1db769ca9498fd9a2091b05f11b19d5087b96bcb
alexjhan <alex.222geri@gmail.com>
2023-10-13 14:51:05 -0500

    Se termino los ejercicios de M03 funciones

---
+++ funciones.js
@@ -0,0 +1,10 @@
+// Función: suma tres
+    function sumaTres(x) {
+        return (x + 3);
+    }
+
+// Función: Cuidado con el console log
+    function cuidadoConElConsoleLog(params) {
+        console.log(nombre);
+        return (nombre);
+    }
