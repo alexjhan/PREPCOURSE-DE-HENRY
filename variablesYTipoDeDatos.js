c0690ea259cee6fac8f766a2ccbe504c1273e1ee
alexjhan <alex.222geri@gmail.com>
2023-10-13 14:19:42 -0500

    se termino los ejerccios M03

--- variablesYTipoDeDatos.js
+++ variablesYTipoDeDatos.js
@@ -1,1 +1,18 @@
-fr
+// variables tipo String (palabras)
+    var bootcamp = "Henry";
+
+// variables tipo Number (numeros)
+    var numeroEntero = 17;
+    var numeroNegativo = -4;
+    var numeroDecimal = 2.34;
+
+// Booleanos
+    var estosEntendiendo = true;
+    var estoyAburrido = false;
+
+// Variables indefinidas
+    var cajon1;
+    console.log(cajon1);
+
+// Null
+    var sinNada = null;
