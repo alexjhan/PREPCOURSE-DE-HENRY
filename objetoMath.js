b2cdc87bd4b75067af1a5b363160cce22a25ec74
alexjhan <alex.222geri@gmail.com>
2023-10-15 12:12:06 -0500

    Se termino los ejercicios de M03 objeto Math.

---
+++ objetoMath.js
@@ -0,0 +1,18 @@
+// Math.pow permite poneciar un numero Math.pow(base, exponente).
+    console.log (Math.pow (2, 3));
+
+// Math.round Math.floor y Math.ceil 
+    // Math.round redondea al numero decimala mas cercano.
+        console.log (Math.round (0.49));
+        console.log (Math.round (0.5));
+    // Math floor redondea el numero decimal al entero de menor valor.
+        console.log (Math.floor (5.93));
+    // Math ceil redondea el numero decimal al entero de mayor valor.
+        console.log (Math.ceil (3.27));
+
+// Math.max y Math.min nos permite con cocer el mayor y menor numero de un conjunto de numeros.
+    console.log (Math.max (1, 2, 3, 6, 4, 5,));
+    console.log (Math.min(3, 4, 5, 6, 1));
+
+// Math.random() nos permite generar un numero aleatorio entre el 0 y 1.
+    console.log (Math.random());
