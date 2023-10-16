c291dc991a432a11d9406decb97e2e68102aa842
alexjhan <alex.222geri@gmail.com>
2023-10-16 12:25:07 -0500

    Se termino los ejercicios del M04 bucles for y while

---
+++ buclesForWhile.js
@@ -0,0 +1,15 @@
+// bucle for presenta la siguiente forma:
+// for (inicializacion, condicion(veces que se repite el bucle), avance)
+let suma = 0;
+for (let i = 0; i < 10; i++) {
+    suma = suma + i;
+    console.log('Variable de iteración: ', i);
+ }
+ console.log('Variable suma: ', suma);
+
+ // Bucle while se utiliza cuando no tenemos con certeza  los pasos que se necesitan hasta finalizar la ejecucion.
+ let suma2 = 0;
+ while(suma2 < 3) {
+    suma2 = suma2 + 1;
+        console.log(suma2);
+    }
