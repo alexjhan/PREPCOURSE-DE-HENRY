898a62a7866f878172c7441ad84de646e003c8be
alexjhan <alex.222geri@gmail.com>
2023-10-16 12:45:05 -0500

    Se terminó los ejrcicios del M05 introduccion a los arreglos.

---
+++ introduccionALosArreglos.js
@@ -0,0 +1,14 @@
+// Lista de compras ejemplo de arreglos, las indices, y los elemntos.
+var listaDeCompras = [];
+listaDeCompras[3] = 'Tomates';
+listaDeCompras[1] = 'Lechuga';
+    console.log (listaDeCompras);
+
+// Elemento del array
+var nombres = ['Matias', 'Maria', 'Diego', 'Rosa'];
+var elementoDelArray = nombres[1];
+console.log (elementoDelArray);
+
+// Lenght
+var nombres = ['Matias', 'Maria', 'Diego', 'Rosa'].length;
+console.log (nombres);
