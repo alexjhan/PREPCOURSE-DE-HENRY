5a53516979134292d91378faf0b117f7416e41f5
alexjhan <alex.222geri@gmail.com>
2023-10-16 19:45:45 -0500

    Se terminó ejercicio del M06 objetos.

---
+++ objetos.js
@@ -0,0 +1,6 @@
+var deportes = { 
+    conBalon: ["Football", "Basketball", "Rugby"],
+    sinBalon: ["Boxeo", "Surf", "Trekking"],
+};
+
+var persona = {nombre: "Lucas", edad: 26, estudions: {esProgramador: true}};
